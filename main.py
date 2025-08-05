c5e060aeb3e9a4942c655e66d42689c9f74e0c00
Pranav Dasan <pranavdasan8@gmail.com>
2025-08-05 14:51:29 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import  numpy as np
+import os
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
+warnings.filterwarnings('ignore')
+
+import tensorflow as tf
+from tensorflow import keras
